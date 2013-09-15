2ff410966135bc3362db903cbbcdf83770344149
Forbes Lindesay <forbes@lindesay.co.uk>
2013-09-15 01:31:04 +0100

    Define missing `javascript` filter

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict'
 
-var jade = require('transform')('jade')
+var jadelib = require('jade')
+var hljs = require('highlight.js')
+var jade = require('transform')('jade', jadelib)
 var express = require('express')
 var app = express()
+
+jadelib.filters.javascript = function (src) {
+  return '<pre><code>' + hljs.highlight('javascript', src).value + '</code></pre>'
+}
 
 app.get('/', jade('./index.jade'))
 app.use(express.static(__dirname))
